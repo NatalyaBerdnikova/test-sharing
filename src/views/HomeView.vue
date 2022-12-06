b64c5f1ea53ae727d77257b27ad96f81140ffef5
Natalya Berdnikova <kitakava@mail.ru>
2022-12-06 10:24:39 +0500

    Add using Open vk API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,49 @@
 <template>
   <div>
-    <h1 class="title">Vue.js 2 Starter by frōntensō</h1>
-    <Picture src="/cat.jpg" alt="" />
+    <div>
+      <button @click="authorizeVk">Авторизироваться вк</button>
+    </div>
+    <div>
+      <button @click="makeRepost">Сделать репост поста</button>
+    </div>
+    <!-- <div id="vk_like">LikeWidget</div> -->
+    <div id="vk_post_-33118207_31542974" />
   </div>
 </template>
 
 <script>
-import Picture from "@/components/Picture";
-
 export default {
   name: "HomeView",
-  components: {
-    Picture,
+  data() {
+    return {
+      user_data: null,
+      postId: "https://vk.com/warface?w=wall-33118207_31542974",
+    };
+  },
+  mounted() {
+    // window.VK.Widgets.Like("vk_like");
+
+    window.VK.Widgets.Post(
+      "vk_post_-33118207_31542974",
+      -33118207,
+      31542974,
+      "abDsTwDfTr9Ep_uoNWXMziQgQpwA"
+    );
+    // It is not working
+    window.VK.Observer.subscribe("widgets.post.shared", () => {
+      console.log("widgets.post.shared");
+      alert("Thank you for your shared.");
+    });
+  },
+  methods: {
+    authorizeVk() {
+      console.log("authorizeVk");
+      this.user_data = window.VK.Auth.login();
+    },
+    makeRepost() {
+      // TODO I can't find desired method
+      window.VK.Api.call("");
+    },
   },
 };
 </script>
