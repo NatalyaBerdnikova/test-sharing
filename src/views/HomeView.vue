a233a371485e21e973dcbe5a25c09f630f1194be
Natalya Berdnikova <kitakava@mail.ru>
2022-12-06 16:54:50 +0500

    Change api version

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,12 +23,12 @@ export default {
   mounted() {
     // window.VK.Widgets.Like("vk_like");
 
-    window.VK.Widgets.Post(
-      "vk_post_-33118207_31542974",
-      -33118207,
-      31542974,
-      "abDsTwDfTr9Ep_uoNWXMziQgQpwA"
-    );
+    // window.VK.Widgets.Post(
+    //   "vk_post_-33118207_31542974",
+    //   -33118207,
+    //   31542974,
+    //   "abDsTwDfTr9Ep_uoNWXMziQgQpwA"
+    // );
     // It is not working
     window.VK.Observer.subscribe("widgets.post.shared", () => {
       console.log("widgets.post.shared");
@@ -46,7 +46,7 @@ export default {
         "stories.save",
         {
           // eslint-disable-next-line
-          v: "5.73",
+          v: "5.131",
           upload_results:
             "https://sun3-12.userapi.com/impg/WhGj_BnshuacWE8dAZvPtoRKQcpzHHtFg5vq8g/veSUdVx3_Ng.jpg?size=1080x1080&quality=95&sign=43524f9d9201c7e961a23fd09eb2a06d&type=album",
         },
